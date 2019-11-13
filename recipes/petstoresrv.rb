029fdc116d77bca72ff551634eafb50212517857
Qing Jiang <qing.j@qingj-m1551.prodege.com>
2019-11-12 16:00:53 -0800

    add limits

--- recipes/petstoresrv.rb
+++ recipes/petstoresrv.rb
@@ -1,3 +1,10 @@
+  ## modify system limits
+  bash "insert_line_to_limits" do
+    code <<-EOH
+        echo "* - nofile 65535" >> /etc/security/limits.conf
+      EOH
+  end
+  
   ## enable and start services
   %w(memcached php7.0-fpm nginx).each do |service|
     service service do
