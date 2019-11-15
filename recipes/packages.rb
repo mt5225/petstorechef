d777b295092e4411e9287e4499f85284413a45ee
Qing Jiang <qing.j@qingj-m1551.prodege.com>
2019-11-15 11:32:36 -0800

    formatter

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,6 +1,6 @@
-packages = node['packages'][node['platform_family']]
+packages = node["packages"][node["platform_family"]]
 
-if node['platform_family'] != 'windows'
+if node["platform_family"] != "windows"
   # loop through packages
   packages.each_key do |package|
     package package do
